42d75b5b1363d45ec2c131f6ce1cb95687e22529
osamuya <oosamuuy@gmail.com>
2018-08-09 15:37:24 +0900

    IPを切り分け

---
+++ approot/public/note/wp-content/themes/note/sysconfig_sample.php
@@ -0,0 +1,17 @@
+<?php
+
+$ip_lists = array(
+		"127.0.0.1", //self
+		"60.64.37.1", //tokura
+		"61.21.62.90", //shibasaki
+		"221.42.149.135",//nishina
+		"126.196.29.106",//tsujido
+		"110.66.186.176",//tsujido
+		"61.126.188.59",//sankaku(main)
+		"153.156.225.210",//sankaku(creative)
+		"119.243.74.222",//sankaku(backup)
+		"153.120.8.50",//japan server
+		"27.133.131.225",//butan server
+		"153.126.138.150",//bangkok server
+		"59.106.223.227",//neojapan server
+);
